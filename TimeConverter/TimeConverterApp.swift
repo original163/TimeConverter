ca1581825060fe5f3f15a3705c2282f67060bd10
Денис Денисов <original163@mail.ru>
2023-10-31 23:34:18 +0400

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Денис Денисов on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
